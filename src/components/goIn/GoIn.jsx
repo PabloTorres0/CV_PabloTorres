269f2d8c8a3f288a5f75d150a558c50fa96856f7
PabloTorres0 <pablo.javier.torres.martinez@gmail.com>
2023-12-05 11:14:35 -0600

    CV sin imagenes

--- src/components/goIn/GoIn.jsx
+++ src/components/goIn/GoIn.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import './goIn.css'
 
-import useSound from 'use-sound';
-import hoverSound from '../../assets/laser.mp3';
 
 const GoIn = (props) => {
-  //sound
-  const [goInTransition, setGoInTransition] = React.useState(false)
-
-
-///
   return (
-    <div className={goInTransition ? 'container__goIn-next' : 'container__goIn'}>
+    <div className={ 'container__goIn'}>
       
         <a href='#mainNav' onClick={()=>{
           props.goInFunction(true)
@@ -20,7 +13,7 @@ const GoIn = (props) => {
         }
         onMouseEnter={()=>{props.handleHover(2)}}
           >
-            {props.idiom ? 'Entrar' :' Go In'}
+            {props.idiom ? 'Go In' :'Entrar'}
         </a>
     </div>
   )
